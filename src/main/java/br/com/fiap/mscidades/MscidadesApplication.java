dabbe8f449b619cabecdf314d101572b5ed617d0
Lucas Godoy <lucas.godoy@sjc.sp.gov.br>
2025-05-09 10:45:24 -0300

    Inicialização do rep

---
+++ src/main/java/br/com/fiap/mscidades/MscidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mscidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscidadesApplication.class, args);
+	}
+
+}
